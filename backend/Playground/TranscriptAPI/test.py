45bf692dd469de69b418a2d5dbd0a07815416b8c
Seema Vora <seema.s.vora@gmail.com>
2021-09-08 15:45:26 -0700

    Playing Around with Transcript API

---
+++ backend/Playground/TranscriptAPI/test.py
@@ -0,0 +1,10 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+#If a video's ID starts with a hyphone you will have to maks the hyphen using a \ to prevent the CLI from mistaking it for a argument name
+
+outls = []
+tx = YouTubeTranscriptApi.get_transcript('d1KcaycYsRs&ab', languages=['de','en'])
+for i in tx:
+    outtxt = (i['text'])
+    outls.append(outtxt)
+    with open ("op2.txt","a") as opf:
+        opf.write(outtxt + "\n")
