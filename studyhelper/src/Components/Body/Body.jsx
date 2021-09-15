848e90b943ac80cacce3e969afbee9e90a409c56
yuki_s <saitoyukisaito@gmail.com>
2021-09-15 04:17:57 -0700

    filled out some text content

--- studyhelper/src/Components/Body/Body.jsx
+++ studyhelper/src/Components/Body/Body.jsx
@@ -4,6 +4,8 @@ import './Body.css';
 import lightBulb from '../../static/img/lightbulb.png';
 import nlp from '../../static/img/nlp.png';
 import calendar from '../../static/img/calendar.png';
+import brain from '../../static/img/brain1.png';
+import gear from '../../static/img/gear1.png';
 
 export const Body = () => {
   return (
@@ -11,10 +13,11 @@ export const Body = () => {
       <div className='body'>
         <div className='about-us'>
           <div className='about-us-text'>
-            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit </h1>
+            <h1>Create smart note cards from Youtube with a single click </h1>
             <p>
-              Soluta repudiandae animi delectus ea cupiditate iure nulla nemo
-              eum aut, quam minima ratione assumenda veniam quas ullam
+              Using neuro-linguistic programming (NLP), important content is
+              identified in a matter of seconds for you. Skip watching hours of
+              media.
             </p>
           </div>
           <div className='quote-showcase'>
@@ -30,71 +33,51 @@ export const Body = () => {
             <div id='content-item-text'>
               <h1>AI Powered</h1>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
-                repudiandae animi delectus ea cupiditate iure nulla nemo eum
-                aut, quam minima ratione assumenda veniam quas ullam. Ipsum
-                ipsam eos doloremque.
+                NLP is based on three fundamental concepts: neurology, language,
+                and function. It is the study of the mind's mental and emotional
+                components in regards to forming and reacting to lingustic
+                characteristcs. These relationships are percieved under the
+                programming model of the mind as an internal operation system.
               </p>
             </div>
           </div>
 
           <div className='content-left content-item'>
             <div>
-              <img src={calendar} alt='' />
+              <img id='calendar' src={calendar} alt='' />
             </div>
             <div id='content-item-text'>
               <h1>Google Calendar</h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
-                repudiandae animi delectus ea cupiditate iure nulla nemo eum
-                aut, quam minima ratione assumenda veniam quas ullam. Ipsum
-                ipsam eos doloremque.
-              </p>
+              <p>Swap this out later for something else if it doesn't happen</p>
             </div>
           </div>
 
           <div className='content-right content-item'>
+            <div>
+              <img src={brain} alt='' />
+            </div>
             <div id='content-item-text'>
               <h1>Convenience is Yours</h1>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
-                repudiandae animi delectus ea cupiditate iure nulla nemo eum
-                aut, quam minima ratione assumenda veniam quas ullam. Ipsum
-                ipsam eos doloremque.
+                All users are one click away from generating a personal
+                collection of smart, interactive note cards.
               </p>
             </div>
           </div>
 
           <div className='content-left content-item'>
-            <div id='content-item-text'>
-              <h1>Additional Tools</h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
-                repudiandae animi delectus ea cupiditate iure nulla nemo eum
-                aut, quam minima ratione assumenda veniam quas ullam. Ipsum
-                ipsam eos doloremque.
-              </p>
+            <div>
+              <img src={gear} alt='' />
             </div>
-          </div>
-
-          <div className='content-right content-item'>
             <div id='content-item-text'>
               <h1>Additional Tools</h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
-                repudiandae animi delectus ea cupiditate iure nulla nemo eum
-                aut, quam minima ratione assumenda veniam quas ullam. Ipsum
-                ipsam eos doloremque.
-              </p>
+              <p>Discord bot or something</p>
             </div>
           </div>
         </div>
         <footer>
           <ul>
-            <li>Lorem ipsum</li>
-            <li>dolor sit</li>
-            <li>amet consectetur</li>
-            <li>adipisicing elit</li>
+            <li>&#169; 2021 Scribe </li>
           </ul>
         </footer>
       </div>
