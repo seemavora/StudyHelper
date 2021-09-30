88a55f1bf05a8c6cac314f17bc9c7e43f5147216
Seema Vora <seema.s.vora@gmail.com>
2021-09-29 18:46:43 -0700

    Adds App.js Changes

--- studyhelper/src/Pages/Homepage/Homepage.js
+++ studyhelper/src/Pages/Homepage/Homepage.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Homepage() {
+    const [currentTime, setCurrentTime] = useState(0);
+
+    useEffect(() => {
+        fetch('/time').then(res => res.json()).then(data => {
+            setCurrentTime(data.time);
+        });
+    }, []);
     return (
         <div className="shopping-list">
-           <h1>Homepage</h1>
+            <h1>Homepage</h1>
+            <p>The current time is {currentTime}.</p>
         </div>
     );
 }
