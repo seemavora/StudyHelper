68a2dbb9676a2346200e7901c26d01e025c51e86
DDimitrov1 <dimitar.dimitrov@sjsu.edu>
2021-11-24 15:23:17 -0800

    login validation implemented, still needs local storage implementation

--- backend/api/flaskmongo/user/models.py
+++ backend/api/flaskmongo/user/models.py
@@ -39,16 +39,53 @@ class User:
 
   def login(self):
     print(request.get_json())
-
     user = {
       "username": request.get_json().get("username"),
       "password": request.get_json().get("password")
     }
 
-    # retrievedUser = mongo.db.users.find(user['username'])
+    # Check for existing username
+    if not (mongo.db.users.find_one({'username' : user["username"]})):
+      #print("user does not exist") #test print
+      return jsonify({
+        "message": 'username does not exist',
+        'error_ID': 'username'
+        }), 400
+
+    #retrieve the existing user data
+    retrieveUser = mongo.db.users.find({'username' : user["username"]})#, {'password': 1, "_id": 0})
+
+    #store the password, ID, and email for future use
+    for key in retrieveUser:
+      password = (key["password"])
+      id = (key["_id"])
+      email = (key["email"])
+
+    #check if the password entered matches the account's password
+    if user['password'] != password:
+      return jsonify({
+        'message': 'Incorrect password',
+        'error_ID': 'pass' 
+        }), 400
+
+    #if user["password"] != password:
+    #  print("invalid password")
+    #else:
+    #  print("valid password")
+
+    #store the ID (or email) in localStorage
+
+    print(id)
+    print(password)
+    print(email)
 
     return jsonify({
       "message": "Hey I got the data",
       "username": user['username'],
       "password": user['password']
       }), 200
+
+#def signout(self):
+#  session.clear()
+  #route for sign out
+#  return redirect('/')
